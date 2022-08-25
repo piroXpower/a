bd26780dc262de0b885ffe5d7548537e98642420
Pikachu <82699010+Gladiator-007@users.noreply.github.com>
2022-08-26 01:43:19 +0530

    Create __main__.py

---
+++ hackingaibot/__main__.py
@@ -0,0 +1,68 @@
+import glob
+import os
+import sys
+from pathlib import Path
+import logging
+import importlib
+from hackingaibot import BotClient, OWNER_ID
+
+#logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
+
+def load_prototype(glad_prototype):
+    path = Path(f"hackingaibot/prototype/{glad_prototype}.py")
+    prototypes = "hackingaibot.prototype.{}".format(glad_prototype)
+    spec = importlib.util.spec_from_file_location(prototypes, path)
+    load = importlib.util.module_from_spec(spec)
+    load.logger = logging.getLogger(glad_prototype)
+    spec.loader.exec_module(load)
+    sys.modules["hackingaibot.prototype." + glad_prototype] = load
+    print("Successfully loaded " + glad_prototype + " prototype.")
+
+
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    pingtime = ""
+    timearray = []
+    timedetail = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        timearray.append(int(result))
+        seconds = int(remainder)
+
+    for i in range(len(timearray)):
+        timearray[i] = str(timearray[i]) + timedetail[i]
+    if len(time_list) == 4:
+        pingtime += timearray.pop() + ", "
+
+    timearray.reverse()
+    pingtime += ":".join(timearray)
+
+    return pingtime
+
+
+
+
+path = "hackingaibot/prototype/*.py"
+files = glob.glob(path)
+for name in files:
+    with open(name) as a:
+        patt = Path(a.name)
+        plugin_name = patt.stem
+        load_prototype(plugin_name.replace(".py", ""))
+
+print("Deployed Hacking-AiBot succesfully!!")
+
+if __name__ == "__main__":
+    if BotClient:
+        BotClient.run_until_disconnected()
+    else:
+        pass
