a496511946415e8710260ab5726dfa7e6d038861
Pikachu <82699010+Gladiator-007@users.noreply.github.com>
2022-08-26 01:57:23 +0530

    DOCUMENTATION LIKH DO VRO

---
+++ hackingaibot/prototype/startbot.py
@@ -0,0 +1,5 @@
+###################### HELP MENU + START COMMAND ONLY #####################
+
+import asyncio
+from hackingaibot import BotClient, StartTime, OWNER_ID, BOT_USERNAME, REPO_NAME
+from telethon import events, custom, Button
